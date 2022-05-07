536bbf42abe78476400f2c9338975cb5747e378a
Darius <ddubose93@gmail.com>
2022-05-07 12:09:01 -0400

    Add project files.

---
+++ BattleshipLibrary/Models/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLibrary.Models
+{
+    public enum GridSpotStatus
+    {
+        Empty = 0,
+        Ship = 1,
+        Miss = 2,
+        Hit = 3,
+        Sunk = 4
+    }
+}
